161a206c53aaed291840a79adcf829b3385d7594
Josh Tate <joshtate0492@gmail.com>
2015-08-07 08:28:09 -0400

    Typo fix

--- lib/colorized_routes/controller.rb
+++ lib/colorized_routes/controller.rb
@@ -1,32 +1,30 @@
-module ColorizedRoutes
-  class Controller
-    # The controller will have many routes
+class ColorizedRoutes::Controller
+  # The controller will have many routes
 
-    def initialize routes, name
-      if name.include? "/"
-        parts = name.split("/")
-        @name = parts.last
-        @namespaces = parts.take(parts.size - 1)
-      else
-        @name = name
-        @namespaces = []
-      end
-      @routes = routes
+  def initialize routes, name
+    if name.include? "/"
+      parts = name.split("/")
+      @name = parts.last
+      @namespaces = parts.take(parts.size - 1)
+    else
+      @name = name
+      @namespaces = []
     end
+    @routes = routes
+  end
 
-    def display
-      space_counter = 0
-      if @namespaces.any?
-        @namespaces.each do |ns|
-          print "#{Array(0..space_counter).map{|s| " "}}"
-          print "Namespace: ".light_white
-          puts " #{ns} ".light_white.on_light_red.bold
-          space_counter += 1
-        end
+  def display
+    space_counter = 0
+    if @namespaces.any?
+      @namespaces.each do |ns|
+        print "#{Array(0..space_counter).map{|s| " "}}"
+        print "Namespace: ".light_white
+        puts " #{ns} ".light_white.on_light_red.bold
+        space_counter += 1
       end
-      print "#{Array(0..space_counter).map{|s| " "}}"
-      print "Controller: ".light_white
-      puts " #{name} ".light_white.on_blue.bold
     end
+    print "#{Array(0..space_counter).map{|s| " "}}"
+    print "Controller: ".light_white
+    puts " #{name} ".light_white.on_blue.bold
   end
 end
--- lib/colorized_routes/route.rb
+++ lib/colorized_routes/route.rb
@@ -1,51 +1,49 @@
-module ColorizedRoutes
-  class Route
-    # The route will hold all the information for the path, action name, etc
+class ColorizedRoutes::Route
+  # The route will hold all the information for the path, action name, etc
 
-    def initialize verb, path, prefix, action
-      @verb = verb
-      @path = path
-      @prefix = prefix
-      @action = action
-    end
+  def initialize verb, path, prefix, action
+    @verb = verb
+    @path = path
+    @prefix = prefix
+    @action = action
+  end
 
-    def display widths
-      print "#{@verb.center(widths[:verb])}".light_red
-      print " | ".light_white
-      print "#{@path.ljust(widths[:path]).split('/').map{|p| p.light_blue}.join('/'.green)}"
-      print " | ".light_white
-      print "#{@prefix.ljust(widths[:prefix])}".yellow
-      print " | ".light_white
-      print "#{@action}".light_green
-    end
+  def display widths
+    print "#{@verb.center(widths[:verb])}".light_red
+    print " | ".light_white
+    print "#{@path.ljust(widths[:path]).split('/').map{|p| p.light_blue}.join('/'.green)}"
+    print " | ".light_white
+    print "#{@prefix.ljust(widths[:prefix])}".yellow
+    print " | ".light_white
+    print "#{@action}".light_green
+  end
 
-    def self.max_widths routes, existing_widths = nil
-      types = [:verb,:path,:prefix,:action]
-      widths = existing_widths.nil? ? Hash[types.map{|t| [t,0]}] : existing_widths
+  def self.max_widths routes, existing_widths = nil
+    types = [:verb,:path,:prefix,:action]
+    widths = existing_widths.nil? ? Hash[types.map{|t| [t,0]}] : existing_widths
 
-      routes.each do |route|
-        types.each do |type|
-          widths[type] = widths[type] < route.send(type) ? widths[type] : route.send(type)
-        end
+    routes.each do |route|
+      types.each do |type|
+        widths[type] = widths[type] < route.send(type) ? widths[type] : route.send(type)
       end
-
-      widths
     end
 
-    def verb
-      @verb
-    end
+    widths
+  end
 
-    def path
-      @path
-    end
+  def verb
+    @verb
+  end
 
-    def prefix
-      @prefix
-    end
+  def path
+    @path
+  end
 
-    def action
-      @action
-    end
+  def prefix
+    @prefix
+  end
+
+  def action
+    @action
   end
 end
